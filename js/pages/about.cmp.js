801a98adf00f85910d1ba939802aee7ec658f39f
carmit vaknin <carmit.vk@gmail.com>
2021-02-27 23:40:08 +0200

    about

--- js/pages/about.cmp.js
+++ js/pages/about.cmp.js
@@ -3,7 +3,25 @@ import { eventBus } from '../services/event-bus-service.js'
 export default {
     template: `
     <section class="about app-main">
-        <h1 ref="header">About page</h1>
+        <!-- <h1 ref="header">This application will enable the use of email options
+            wrote by:
+        </h1> -->
+
+
+
+          <!-- Me -->
+        <div class="col-spaces flex-align-center">
+            <p>
+                This application will enable the use of email options wrote by
+            </p>
+            <img class="mx-auto rounded-circle" src="../../../img/img1.jpeg" alt="">
+            <h4 >Carmit Vaknin</h4>
+            <p class="about-description">Lives in center of the country. </p>
+            <p class="about-description">like to paint and to learn new things. </p>
+            <p class="about-description">last learned to be a full stack developer. </p>    
+        </div>
+
+
         <!-- <button @click="callBus">Call the bus!</button> -->
     </section>
     `,
